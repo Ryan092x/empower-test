01ec33f2894e8c3f54b74953f746dc3f2018be51
Ryan Victor <ryanvictor.am@gmail.com>
2024-02-21 15:57:05 -0400

    teste

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-var swiper = new Swiper(".slide-content", {
+var swiper1 = new Swiper(".slide-content", {
   slidesPerView: 3,
   spaceBetween: 25,
   loop: true,
-  centerSlide: 'true',
+  centerSlide: "true",
   grabCursor: true,
   fade: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swp1",
     clickable: true,
     dynamicBullets: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next-slider1",
+    prevEl: ".swiper-button-prev-slider1",
   },
 
   breakpoints: {
@@ -27,3 +27,105 @@ var swiper = new Swiper(".slide-content", {
     },
   },
 });
+
+var swiper4 = new Swiper(".coluna", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  loop: true,
+  centerSlide: "true",
+  fade: true,
+  speed:800,
+  grabCursor: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  effect: 'slide',
+  allowTouchMove: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    420: {
+      slidesPerView: 2,
+    },
+    470: {
+      slidesPerView: 3,
+    },
+    540: {
+      slidesPerView: 4,
+    },
+    980: {
+      slidesPerView: 3,
+    },
+  }, 
+});
+
+var swiper3 = new Swiper(".eventos-cards", {
+  slidesPerView: 1,
+  grabCursor: true,
+  fade: true,
+  loop: true,
+  spaceBetween: 40,
+  pagination: {
+    el: ".swp2",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-slider2",
+    prevEl: ".swiper-button-prev-slider2",
+  },
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+});
+
+function search() {
+  let input = document.getElementById('searchbar').value.trim().toLowerCase();
+  let cards = document.querySelectorAll('.card');
+
+  let matchesCount = 0; // Contador para o número de cartões correspondentes
+  cards.forEach(card => {
+    let name = card.querySelector('.name').textContent.toLowerCase();
+    let description = card.querySelector('.description').textContent.toLowerCase();
+    let matches = name.includes(input) || description.includes(input);
+    if (matches) {
+      card.style.display = 'block';
+      matchesCount++;
+    } else {
+      card.style.display = 'none';
+    }
+  });
+
+  // Ajusta o número de slides exibidos com base no número de correspondências
+  let slidesPerView = Math.min(matchesCount, 3); // Mostra no máximo 3 slides
+  swiper1.params.slidesPerView = slidesPerView;
+  swiper1.update(); // Atualiza o Swiper
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buscarButton').addEventListener('click', search);
+
+  // Captura o evento "submit" do formulário
+  document.querySelector('.buscar-palestrante').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do formulário (ou seja, enviar e atualizar a página)
+    search(); // Chama a função de busca quando o formulário é enviado
+  });
+});
+
+// Perguntas Frequentes
+
+function togglePergunta(pergunta) {
+  var perguntaAtual = pergunta.parentNode;
+  var todasPerguntas = document.getElementsByClassName('pergunta');
+  
+  for (var i = 0; i < todasPerguntas.length; i++) {
+    if (todasPerguntas[i] !== perguntaAtual) {
+      todasPerguntas[i].classList.remove('active');
+    }
+  }
+
+  perguntaAtual.classList.toggle('active');
+}
